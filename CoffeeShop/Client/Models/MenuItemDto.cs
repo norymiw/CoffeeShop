bba84b0cd617dbf1acb533c55c95f5f439c21170
norymiw <shuramaruru906@inbox.ru>
2025-05-30 02:59:57 +0300

    Add files via upload

---
+++ CoffeeShop/Client/Models/MenuItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CoffeeShop.Client.Models
+{
+    public class MenuItemDto
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public decimal Price { get; init; }
+        public string Category { get; init; }
+        public int Calories { get; init; }
+
+        public string ImageUrl { get; init; } //добавила
+    }
+}
